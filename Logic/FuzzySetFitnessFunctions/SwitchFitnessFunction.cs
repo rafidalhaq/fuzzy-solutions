489166d375937246ebc0f28d73a1de12e1bb41a8
shtaket <shtaket@localhost>
2010-12-14 00:24:29 +0300

    remove fictive code

--- Logic/FuzzySetFitnessFunctions/SwitchFitnessFunction.cs
+++ Logic/FuzzySetFitnessFunctions/SwitchFitnessFunction.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using IGS.Fuzzy.Core;
 
 namespace IGS.Fuzzy.FitnessFunctions
@@ -32,10 +31,5 @@ namespace IGS.Fuzzy.FitnessFunctions
         }
 
         #endregion
-
-        public void OnUniversalItemsCollectionChanged(object sender, UniversalItemsEventArgs<T> universalItemsEventArgs)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
