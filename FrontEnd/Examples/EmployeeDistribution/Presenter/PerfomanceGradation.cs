6807d6477fe7cd6b26d2b7b0e7770bf970624a10
glebi <glebi@localhost>
2010-12-19 01:12:01 +0500

    ???????? ????? ?????????????? ???????? ?????????????????? ??????????

---
+++ FrontEnd/Examples/EmployeeDistribution/Presenter/PerfomanceGradation.cs
@@ -0,0 +1,14 @@
+﻿namespace Presenter
+{
+    public class PerfomanceGradation
+    {
+        public PerfomanceGradation(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+
+        public double Desirability { get; set; }
+    }
+}
