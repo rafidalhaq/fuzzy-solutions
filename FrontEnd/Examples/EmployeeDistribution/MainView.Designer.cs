deeec484e0a2aae08645cdec2783c6b5836db055
glebi <glebi@localhost>
2010-12-19 01:11:38 +0500

    ????????? ????????????? ??? ????????? ???????

---
+++ FrontEnd/Examples/EmployeeDistribution/MainView.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace EmployeeDistribution
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonNext = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dataGrid.Location = new System.Drawing.Point(12, 12);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(674, 431);
+            this.dataGrid.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Работники";
+            this.Column1.Name = "Column1";
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(611, 474);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 1;
+            this.buttonNext.Text = "Далее";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.ButtonNextClick);
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 509);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.dataGrid);
+            this.Name = "MainView";
+            this.Text = "MainView";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button buttonNext;
+    }
+}
---
+++ FrontEnd/Examples/EmployeeDistribution/MainView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using Presenter;
+
+namespace EmployeeDistribution
+{
+    public partial class MainView : Form
+    {
+        private IEmployeeDistributionPresenter presenter;
+
+        public MainView(IEmployeeDistributionPresenter employeeDistributionPresenter)
+        {
+            presenter = employeeDistributionPresenter;
+            InitializeComponent();
+        }
+
+        private void ButtonNextClick(object sender, EventArgs e)
+        {
+            var employee = dataGrid.Rows
+                .Cast<DataGridViewRow>()
+                .Select(row => row.Cells[0].EditedFormattedValue.ToString())
+                .ToList();
+
+            dataGrid.Columns.Clear();
+
+            var dataGridViewColumn = new DataGridViewColumn
+                                         {
+                                             AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells,
+                                             ReadOnly = true
+                                         };
+
+            dataGrid.Columns.Add(dataGridViewColumn);
+            foreach (var perfomanceGradation in presenter.PerfomanceGradations)
+                dataGrid.Columns.Add(perfomanceGradation.Name, perfomanceGradation.Name);
+
+            foreach (var emp in employee)
+            {
+                var dataGridViewRow = new DataGridViewRow();
+                dataGrid.Rows.Add(dataGridViewRow);
+
+            }
+        }
+    }
+}
