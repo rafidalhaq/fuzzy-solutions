a5b4047d94373acf4ec587a6cd0bed439b98fc2c
glebi <glebi@localhost>
2010-12-18 21:00:27 +0500

    ???????? ???????? ?????? "????????????? ???????????"

---
+++ FrontEnd/Examples/EmployeeDistribution/PresenterTests/PresenterTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PresenterTests
+{
+    public class PresenterTests
+    {
+        [Xunit.Fact]
+        public void NothingToTestYet()
+        {
+            
+        }
+    }
+}
