389f399e2a05a89196719edf2151850fc1a48d97
shtaket <shtaket@localhost>
2011-05-11 23:27:16 +0400

    ??????? ?????? ?????????????? ???-???????, ??????? ? ?????? ???????? ?????? ? ????????? ??????

---
+++ Logic/MathExtensions/MathExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace IGS.MathExtensions
+{
+    public static class MathExtensions
+    {
+        /// <summary>
+        /// Returns 1 if first less or equal than second, or returns second
+        /// </summary>
+        public static double Truncation(double first, double second)
+        {
+           return first <= second ?  1 : second;
+        }
+
+        /// <summary>
+        /// Returns 1 if first less than second, or returns second
+        /// </summary>
+        public static int LaxTruncation(int first, int second)
+        {
+            return first < second ? 1 : second;
+        }
+    }
+}
---
+++ Tests/LogicTests/MathExtensionsTests/BinaryOperationsTests.cs
@@ -0,0 +1,24 @@
+﻿using IGS.MathExtensions;
+using Xunit;
+
+namespace MathExtensionsTests
+{
+    public class BinaryOperationsTests
+    {
+        [Fact]
+        public void TestTruncation()
+        {
+            Assert.Equal(1, MathExtensions.Truncation(3, 4));
+            Assert.Equal(1, MathExtensions.Truncation(4, 4));
+            Assert.Equal(3, MathExtensions.Truncation(4, 3));
+        }
+
+        [Fact]
+        public void TestLaxTruncation()
+        {
+            Assert.Equal(1, MathExtensions.LaxTruncation(3, 4));
+            Assert.Equal(4, MathExtensions.LaxTruncation(4, 4));
+            Assert.Equal(3, MathExtensions.LaxTruncation(4, 3));
+        }
+    }
+}
